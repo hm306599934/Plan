156b2aae1a7799ea7f699e4f7a0ce38a2a6658ca
jesse <2851670157@qq.com>
2015-02-08 14:29:22 +0800

    ll

---
+++ Plan/MainCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MainCollectionViewCell.swift
+//  Plan
+//
+//  Created by Tony on 15/2/6.
+//  Copyright (c) 2015年 Jesse. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var viewBackground: UIView!
+    @IBOutlet weak var viewTime: UIView!
+    
+}
