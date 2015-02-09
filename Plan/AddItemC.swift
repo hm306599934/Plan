89115278d652b52fc12e30cd04650024eff5085b
jesse <2851670157@qq.com>
2015-02-09 10:34:39 +0800

    ss

---
+++ Plan/AddItemC.swift
@@ -0,0 +1,20 @@
+//
+//  ClickDetail.swift
+//  Plan
+//
+//  Created by Tony on 15/2/5.
+//  Copyright (c) 2015年 Jesse. All rights reserved.
+//
+
+import UIKit
+
+class AddItemC: UIViewController {
+    
+    @IBOutlet weak var mTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
